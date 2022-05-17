55b3634c4a120addb832e007a47ba9ead39460a4
Brian White <brian@thirdember.com>
2022-05-17 14:17:22 -0400

    add runpace folder with html & js

---
+++ runpace/calc.js
@@ -0,0 +1,82 @@
+class PaceCalc {
+    constructor(MinutesPerMileAsHMS) {
+        this._InputPaceHMS = MinutesPerMileAsHMS;
+        this._SecondsPerMile = PaceCalc.msStringToSeconds(MinutesPerMileAsHMS);
+        this._SecondsPerKilometer = this._SecondsPerMile / PaceCalc.mileToKMConversionConstant;
+    }
+    get MinutesPerKilometerFormatted() {
+        return PaceCalc.formatDurationFromSeconds(this._SecondsPerKilometer);
+    }
+    get MinutesPerMileOriginal() {
+        return this._InputPaceHMS.replace('.', ':');
+    }
+    get MinutesPerMileFormatted() {
+        return PaceCalc.formatDurationFromSeconds(this._SecondsPerMile);
+        ;
+    }
+    get Estimated5KTotalTimeFormatted() {
+        return PaceCalc.formatDurationFromSeconds(this._SecondsPerKilometer * 5);
+    }
+    get Estimated5KTotalTimeMinutesDecimal() {
+        return (this._SecondsPerKilometer * 5) / 60;
+    }
+    get Estimated10TotalTimeFormatted() {
+        return PaceCalc.formatDurationFromSeconds(this._SecondsPerKilometer * 10);
+    }
+    static getNumber(stringValue) {
+        const num = parseInt(stringValue);
+        return isNaN(num) ? 0 : num;
+    }
+    static msStringToSeconds(mmss) {
+        let seconds = 0;
+        mmss = mmss.replace(':', '.');
+        const timeParts = mmss.split('.');
+        if (timeParts.length == 1) {
+            seconds = this.getNumber(timeParts[0]) * 60;
+        }
+        else if (timeParts.length == 2) {
+            let secondsString = timeParts[1] + "00";
+            secondsString = secondsString.slice(0, 2);
+            seconds = (this.getNumber(timeParts[0]) * 60) + this.getNumber(secondsString);
+        }
+        return seconds;
+    }
+    static formatDurationFromSeconds(seconds) {
+        // 502 seconds
+        const minutesDecimal = seconds / 60;
+        // 8.36666667
+        const minutesPortion = Math.floor(minutesDecimal);
+        // 8
+        const secondsDecimal = (minutesDecimal - minutesPortion) * 60;
+        // 22.000002
+        const secondsPortion = Math.floor(secondsDecimal);
+        // 22
+        // let timeString = new Date(seconds * 1000).toISOString().substring(11, 11 + 8);
+        // timeString = timeString.replace(/^0(?:0:0?)?/, '');
+        const secondsPadded = String(secondsPortion).padStart(2, '0');
+        //2 = 02
+        return `${minutesPortion}:${secondsPadded}`;
+    }
+    static calcPaceFromTotalTime(totalSeconds, km) {
+        const secondsPerKM = totalSeconds / km;
+        const secondsPerMile = secondsPerKM * this.mileToKMConversionConstant;
+        return this.formatDurationFromSeconds(secondsPerMile);
+    }
+    static RoundUpToNearest(passednumber, roundto) {
+        if (roundto == 0) {
+            return passednumber;
+        }
+        else {
+            return Math.ceil(passednumber / roundto) * roundto;
+        }
+    }
+    static RoundDownToNearest(passednumber, roundto) {
+        if (roundto == 0) {
+            return passednumber;
+        }
+        else {
+            return Math.floor(passednumber / roundto) * roundto;
+        }
+    }
+}
+PaceCalc.mileToKMConversionConstant = 1.609344;
