8d0ce6050bd287ec0cdbb7c4f7adb8341f8f6ef8
Brian White <brian@thirdember.com>
2022-07-03 14:07:13 -0400

    add tip calc pages

---
+++ tipcalc/tipcalc.js
@@ -0,0 +1,73 @@
+class TipCalcResult {
+    constructor() {
+        this.originalTotal = 0;
+        this.tip = 0;
+        this.totalAmount = 0;
+        this.actualPercent = 0;
+    }
+}
+class TipCalc {
+    constructor(TotalAmount) {
+        this._InputValue = "";
+        this._OriginalTotal = 0;
+        this._TargetPercent = 0;
+        this._ActualPercent = 0;
+        this._TipAmount = 0;
+        this._Nearest = 0;
+        this._InputValue = TotalAmount;
+        this._OriginalTotal = this.getNumber(TotalAmount);
+    }
+    get OriginalTotal() {
+        return this._OriginalTotal;
+    }
+    get TipAmount() {
+        return this._TipAmount;
+    }
+    get TotalAmount() {
+        return this._OriginalTotal + this._TipAmount;
+    }
+    get TargetPercent() {
+        return this._TargetPercent;
+    }
+    get ActualPercent() {
+        let pct = (this._ActualPercent * 100).toFixed(2);
+        return Number(pct);
+    }
+    get ActualPercentFormatted() {
+        return this.ActualPercent + "%";
+    }
+    get Nearest() {
+        return this._Nearest;
+    }
+    getNumber(stringValue) {
+        const num = parseFloat(stringValue);
+        return isNaN(num) ? 0 : num;
+    }
+    roundToTwo(num) {
+        return Math.round((num + Number.EPSILON) * 100) / 100;
+    }
+    createResultJson() {
+        let result = new TipCalcResult();
+        result.originalTotal = this._OriginalTotal;
+        result.tip = this._TipAmount;
+        result.totalAmount = this.TotalAmount;
+        result.actualPercent = this.ActualPercent;
+        return result;
+    }
+    calculateTip(percent, nearest = 0) {
+        let floatPercent = percent / 100;
+        this._TargetPercent = percent;
+        this._Nearest = nearest;
+        let tip = 0;
+        tip = this._OriginalTotal * floatPercent;
+        if (nearest > 0) {
+            tip = Math.ceil(tip / nearest) * nearest;
+        }
+        else {
+            tip = this.roundToTwo(tip);
+        }
+        this._TipAmount = tip;
+        this._ActualPercent = tip / this._OriginalTotal;
+        return this.createResultJson();
+    }
+}
