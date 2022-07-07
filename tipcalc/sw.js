cbbf5f6f8472c5a00d736b43031be9e3986c97d1
Brian White <brianmwhite@users.noreply.github.com>
2022-07-07 10:22:40 -0400

    follow offline mode tutorial

---
+++ tipcalc/sw.js
@@ -0,0 +1,9 @@
+var GHPATH = '/tipcalc';
+var APP_PREFIX = 'brianmwhitetipcalc_';
+var VERSION = 'version_00';
+var URLS = [    
+  '${GHPATH}/',
+  '${GHPATH}/styles.css',
+  '${GHPATH}/index.html',
+  '${GHPATH}/tipcalc.js'
+]
