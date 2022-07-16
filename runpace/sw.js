7cbc5630db83c528064c146ef0573af6e58ccb78
Brian White <brian@thirdember.com>
2022-07-16 11:11:51 -0400

    add serviceworker for offline support

---
+++ runpace/sw.js
@@ -0,0 +1,9 @@
+var GHPATH = '/runpace';
+var APP_PREFIX = 'brianmwhiterunpace_';
+var VERSION = 'version_01';
+var URLS = [
+  '${GHPATH}/',
+  '${GHPATH}/styles.css',
+  '${GHPATH}/index.html',
+  '${GHPATH}/calc.js'
+]
