353198e889a4e53059d3797aaf34ca5d5b5a0514
Brian White <brian@thirdember.com>
2022-07-17 18:03:18 -0400

    use google tutorial for pwa offline to make changes: https://developers.google.com/codelabs/pwa-training/pwa03--going-offline#3

--- tipcalc/sw.js
+++ tipcalc/sw.js
@@ -1,9 +1,32 @@
-var GHPATH = '/tipcalc';
-var APP_PREFIX = 'brianmwhitetipcalc_';
-var VERSION = 'version_04';
-var URLS = [
-  '${GHPATH}/',
-  '${GHPATH}/styles.css',
-  '${GHPATH}/index.html',
-  '${GHPATH}/tipcalc.js'
-]
+const cacheName = 'tipcalc-v1.01';
+
+const precacheResources = [
+  './',
+  './index.html',
+  './styles.css',
+  './tipcalc.js',
+  './main.js',
+  './apple-touch-icon.png',
+  './manifest.webmanifest',
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
