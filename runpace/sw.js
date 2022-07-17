bdef1e73eae5d3c44cff602d61a2bb92440b834a
Brian White <brian@thirdember.com>
2022-07-17 19:14:59 -0400

    update offline mode sw code to match tipcalc changes

--- runpace/sw.js
+++ runpace/sw.js
@@ -1,9 +1,32 @@
-var GHPATH = '/runpace';
-var APP_PREFIX = 'brianmwhiterunpace_';
-var VERSION = 'version_01';
-var URLS = [
-  '${GHPATH}/',
-  '${GHPATH}/styles.css',
-  '${GHPATH}/index.html',
-  '${GHPATH}/calc.js'
-]
+const cacheName = 'runpace-v1.01';
+
+const precacheResources = [
+  './',
+  './index.html',
+  './styles.css',
+  './calc.js',
+  './main.js',
+  './apple-touch-icon.png',
+  './manifest.webmanifest',
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
