f0c5973b312fffc17b868965a5aaa78a68b8f19f
Brian White <brian@thirdember.com>
2023-04-25 14:33:28 -0400

    attempt to add version on the ui and remove cache when new version is

--- runpace/sw.js
+++ runpace/sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'runpace-v2.02';
+const cacheName = 'runpace-v2.06';
 
 const precacheResources = [
   './',
@@ -17,6 +17,23 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   console.log('Service worker activate event!');
+  //check if there is a new version of the app and clear the local storage
+  sendMessageWithVersion(event);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+          return cacheName.startsWith('runpace-');
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        }),
+      );
+    }).then(function () {
+      return self.clients.claim();
+    }));
 });
 
 self.addEventListener('fetch', (event) => {
@@ -29,4 +46,27 @@ self.addEventListener('fetch', (event) => {
       return fetch(event.request);
     }),
   );
+  sendMessageWithVersion(event);
 });
+
+function sendMessageWithVersion(event) {
+  event.waitUntil(
+    (async () => {
+      // Exit early if we don't have access to the client.
+      // Eg, if it's cross-origin.
+      if (!event.clientId) return;
+
+      // Get the client.
+      const client = await clients.get(event.clientId);
+      // Exit early if we don't get the client.
+      // Eg, if it closed.
+      if (!client) return;
+
+      // Send a message to the client.
+      client.postMessage({
+        msg: cacheName,
+        url: event.request.url,
+      });
+    })()
+  );
+}
